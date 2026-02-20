bb47639a65985c49cc4a0e96b6a0bc96ffaa8790
ukukdin <eum714211@gmail.com>
2026-02-20 16:03:06 +0900

    solve: 33846

---
+++ ukukdin/33846.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int t = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+
+        for (int i = 1; i < t; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i =0; i < n; i++){
+            sb.append(arr[i]);
+            if (i < n - 1) sb.append(" ");
+        }
+        bw.write(sb.toString());
+        bw.newLine();
+        bw.flush();
+        bw.close();
+
+    }
+}
